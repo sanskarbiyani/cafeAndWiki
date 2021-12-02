3611fe45694b9b13b8f5b8b7d29a886907742bc6
Sanskar Biyani <sanskarbiyani902@gmail.com>
2021-12-02 11:01:55 +0530

    Filter added to side of cafe Options page

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, redirect, request, make_response
-from flask.helpers import send_from_directory
+from flask.helpers import flash, send_from_directory
 from flask_bootstrap import Bootstrap
 from bson import json_util
 from bson.objectid import ObjectId
@@ -20,10 +20,8 @@ client = pymongo.MongoClient("mongodb://localhost:27017")
 db = client["coffeeAndWiki"]
 
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/")
 def get_all_cafe():
-    if request.method == "POST":
-        print(f"Search Query: {request.form['q']}")
     cafes = db.cafe.find({})
     cafe_list = []
     for cafe in cafes:
@@ -51,162 +49,38 @@ def get_picture(name):
 
 @app.get("/search")
 def search():
-    q = request.args.getlist('query')[0]
-    search_parameter = request.args.getlist('parameter')[0]
-    cafe_list = []
-    if search_parameter == "Name":
-        cursor = db.cafe.find({"name": q})
+    print(request.full_path)
+    if request.args.getlist('query'):
+        q = request.args.getlist('query')[0]
+        search_parameter = request.args.getlist('parameter')[0]
+        print(f'Query: {q}\t\tParameter: {search_parameter}')
+        cafe_list = []
+        # Matches if word is present inside the statement.
+        if search_parameter == "Name":
+            cursor = db.cafe.find({"name": {"$regex": f".*{q}.*"}})
+        else:
+            cursor = db.cafe.find(
+                {f"address.{search_parameter.lower()}": {"$regex": f".*{q}.*"}})
+        for cafe in cursor:
+            cafe_record = {
+                "name": cafe["name"],
+                "id": str(cafe["_id"]),
+                "address": cafe["address"]
+            }
+            cafe_list.append(cafe_record)
+        return render_template('index.html', cafe_list=cafe_list, show_landing=False)
     else:
-        cursor = db.cafe.find({f"address.{search_parameter.lower()}": q})
-    for cafe in cursor:
-        cafe_record = {
-            "name": cafe["name"],
-            "id": str(cafe["_id"]),
-            "address": cafe["address"]
-        }
-        cafe_list.append(cafe_record)
-    return render_template('index.html', cafe_list=cafe_list, show_landing=False)
+        cafes = db.cafe.find({})
+        cafe_list = []
+        for cafe in cafes:
+            cafe_record = {
+                "name": cafe["name"],
+                "id": str(cafe["_id"]),
+                "address": cafe["address"]
+            }
+            cafe_list.append(cafe_record)
+        return render_template('index.html', cafe_list=cafe_list, show_landing=False)
 
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000, debug=True)
-
-# cafe_list = [
-#     {
-#         "name": "Happy's Cafe",
-#         "address": {
-#             "main": "304, Beverly Park",
-#             "locality": "Mira Road",
-#             "city": "Mumbai",
-#             "state": "Maharashtra",
-#             "country": "India"
-#         },
-#         "contact": {
-#             "email": "happycafe@gmail.com",
-#             "phone": 4984865515,
-#             "website": "xyz@email.com"
-#         },
-#         "owner": "Sanskar Biyani",
-#         "established": datetime(2021, 2, 14),
-#         "features": {
-#             "socket": "Yes",
-#             "socketRating": 4,
-#             "wifi": "Yes",
-#             "wifiRating": 3
-#         }
-#     },
-#     {
-#         "name": "Rahul Cafe",
-#         "address": {
-#             "main": "304, Beverly Park",
-#             "locality": "Mira Road",
-#             "city": "Mumbai",
-#             "state": "Maharashtra",
-#             "country": "India",
-#             "near": "Hanuman Mandir"
-#         },
-#         "contact": {
-#             "email": "rahulcafe@gmail.com",
-#             "phone": 4984865515,
-#             "website": "xyz@email.com"
-#         },
-#         "owner": "Sanskar Biyani",
-#         "established": datetime(2021, 2, 14),
-#         "features": {
-#             "socket": "Yes",
-#             "socketRating": 4,
-#             "wifi": "Yes",
-#             "wifiRating": 3
-#         }
-#     },
-#     {
-#         "name": "Cafe Nostalgia",
-#         "address": {
-#             "main": "1st Floor, Building no 2, Vardhaman Complex, Golden Nest",
-#             "locality": "Bhayander",
-#             "city": "Mumbai",
-#             "state": "Maharashtra",
-#             "country": "India",
-#             "near": "Hanuman mandir"
-#         },
-#         "contact": {
-#             "email": "cafenostalgia@gmail.com",
-#             "phone": 4984865515,
-#             "website": "xyz@email.com"
-#         },
-#         "owner": "Sanskar Biyani",
-#         "established": datetime(2021, 2, 14),
-#         "features": {
-#             "socket": "Yes",
-#             "socketRating": 4,
-#             "wifi": "Yes",
-#             "wifiRating": 3
-#         }
-#     }
-# ]
-
-# db.cafe.insert_many(cafe_list)
-# db.cafe.update_one(
-#     {"_id": ObjectId("61827e3443c0e2ba73195726")}, {"$set": {"description": "a pellentesque sit amet porttitor eget dolor morbi non arcu risus quis varius quam quisque id diam vel quam elementum"}})
-
-# pprint(db.cafe.find_one({"_id": ObjectId("61827e3443c0e2ba73195726")}))
-
-# menu = {
-#     "Burning Beverage": {
-#         "Americano(Black Coffe)": 100,
-#         "Cappuccino": 100,
-#         "Latte": 100,
-#         "Hot Chocolate": 100,
-#         "Mocha": 100,
-#         "Irish Coffee": 100
-#     },
-#     "Hot Teas": {
-#         "Masala Tea": 100,
-#         "Ginger Tea": 100,
-#         "Black Tea": 100,
-#         "Irani Tea": 100,
-#         "Lemon Ginger Tea": 100,
-#         "Mint Tea": 100,
-#         "Green Tea": 100
-#     },
-#     "Cafe Frappers": {
-#         "Cold Coffee": 140,
-#         "Irish Delight": 160,
-#         "Mocha Shake": 160,
-#         "Rich Coffee": 160,
-#         "Brownie Coffee": 180,
-#         "Oreo Shake": 180,
-#         "Kitkat Shake": 180,
-#         "Cookie Shake": 180
-#     },
-#     "Ice Teas": {
-#         "Lemon Ice Tea": 120,
-#         "Peach Ice Tea": 120,
-#         "Cranberry Tea": 120,
-#         "Mint Ice Tea": 120,
-#         "Watermelon Ice Tea": 120
-#     },
-#     "Lemonades": {
-#         "Classic lemonade": 100,
-#         "Kiwi Lemonade": 120,
-#         "Watermelon Lemonade": 120,
-#         "Ocean Lemonade": 120,
-#         "Masala Lemonade": 120
-#     },
-#     "Mocktales": {
-#         "Lost Island": 160,
-#         "Mojito": 140,
-#         "Green Island": 140,
-#         "Blue Nirvana": 140,
-#         "Green Apple": 140
-#     },
-#     "Deserts": {
-#         "Sizzling Browning": 160,
-#         "Coffee Afogate": 120,
-#         "Strawberry Delight": 140,
-#         "Brownie Delight": 140,
-#         "Coffee Chocolate Sunday": 140
-#     }
-# }
-
-# db.cafe.update_one({"name": "Cafe Nostalgia"}, {"$set": {"menu": menu}})
--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,6 @@
+window.addEventListener('load', openSearch);
+document.queryForm.reset.addEventListener('click', removeParameter);
+
 function displayCafe(){
     const cafeContainer = document.getElementById("cafe-container");
     cafeContainer.style.display = "flex";
@@ -5,6 +8,7 @@ function displayCafe(){
     landingElem.style.display = "none";
 }
 
+// Go back to Home page
 function back(){
     const landingElem = document.getElementById("landing-page");
     landingElem.style.display = "flex";
@@ -13,6 +17,7 @@ function back(){
 }
 
 function openSearch(){
+    // Automaticallu fill the form with the contents of the previous search.
     const url = window.location.href.split("?");
     if (url.length == 2){
         const urlParameters = url[1].split("&");
@@ -20,12 +25,6 @@ function openSearch(){
         urlParameters.forEach(param => {
             values.push(param.split("=")[1]);
         });
-        // const searchValue = values[0].split("+");
-        // if(searchValue.length == 1){
-        //     document.queryForm.query.value = `${values[0]}`;
-        // } else {
-        //     document.queryForm.query.value = `${searchValue[0]} ${searchValue[1]}`
-        // }
         const searchValue = values[0].split("+").join(" ");
         document.queryForm.query.value = `${searchValue}`;
         let array = document.querySelectorAll(".parameters span");
@@ -35,32 +34,36 @@ function openSearch(){
             }
         })
     }
-    const cafeContainer = document.getElementById("search-container");
-    cafeContainer.style.display= "flex";
-    const landingElem = document.getElementById("landing-page");
-    landingElem.style.filter="brightness(20%)";
-    cafeContainer.style.cursor = "pointer";
-    setTimeout(()=>{
-        cafeContainer.addEventListener('click', backToLanding);
-        cafeContainer.lastElementChild.style.cursor = "default";
-    }, 500);
+    // // To open the search box
+    // const cafeContainer = document.getElementById("search-container");
+    // cafeContainer.style.display= "flex";
+    // const landingElem = document.getElementById("landing-page");
+    // landingElem.style.filter="brightness(20%)";
+    // cafeContainer.style.cursor = "pointer";
+    // setTimeout(()=>{
+    //     cafeContainer.addEventListener('click', backToLanding);
+    //     cafeContainer.lastElementChild.style.cursor = "default";
+    // }, 500);
 }
 
-function backToLanding(elem){
-    // If the event is triggered by the child element, then return immediately.
-    var targetElem = elem.target;
-    if (!(targetElem.tagName == 'SECTION')) return;
+// // Function to go back to landing when clicked on the surrounding area.
+// function backToLanding(elem){
+//     // If the event is triggered by the child element, then return immediately.
+//     var targetElem = elem.target;
+//     if (!(targetElem.tagName == 'SECTION')) return;
     
-    const cafeContainer = document.getElementById("search-container");
-    cafeContainer.style.display= "none";
-    const landingElem = document.getElementById("landing-page");
-    landingElem.style.filter="brightness(100%)";
-    cafeContainer.style.cursor = "default";
-    setTimeout(()=>{
-        cafeContainer.removeEventListener('click', backToLanding);
-    }, 500);
-}
+//     const cafeContainer = document.getElementById("search-container");
+//     cafeContainer.style.display= "none";
+//     const landingElem = document.getElementById("landing-page");
+//     landingElem.style.filter="brightness(100%)";
+//     cafeContainer.style.cursor = "default";
+//     setTimeout(()=>{
+//         cafeContainer.removeEventListener('click', backToLanding);
+//     }, 500);
+// }
 
+// Function to add the parameter to the selected parameter list when selected.
+// Remove any previous parameter present
 function addParameter(element){
     let parameter = element.innerHTML;
     let newParameterElem = document.createElement('span');
@@ -69,24 +72,34 @@ function addParameter(element){
         parent.removeChild(parent.lastElementChild);
     }
     newParameterElem.innerHTML = `${parameter}&nbsp | &nbsp<i class="fas fa-times"></i>`;
-    newParameterElem.addEventListener('click', removeParamter);
+    newParameterElem.addEventListener('click', removeParameter);
     parent.appendChild(newParameterElem);
     document.queryForm.parameter.value = parameter;
     parent.style.display = "flex";
 }
 
-function removeParamter(event){
-    if (event.target.tagName != 'SPAN'){
-        var elem = event.target.parentElement;
+// To remove parameter present in the selected parameters
+function removeParameter(event){
+    if (event.target.tagName == 'INPUT'){
+        var parent = document.getElementsByClassName('search-parameters')[0];
+        var elem = parent.lastElementChild;
     } else {
-        var elem = event.target;
+        if (event.target.tagName != 'SPAN'){
+            var elem = event.target.parentElement;
+        } else {
+            var elem = event.target;
+        }
+        var parent = elem.parentElement;
     }
-    const parent = elem.parentElement;
     parent.removeChild(elem);
     parent.style.display = "none";
-    document.queryForm.parameter.removeAttribute('value');
+    document.queryForm.parameter.value="";
 }
 
+// To check form details
+    // Both the fields should not be empty
+    // Parameter should be any of the one mentioned in the Options
+    // Display the errors if any
 function validate(event){
     const validParameters = ['City', 'Country', 'State', 'Locality', 'Name'];
     let searchString = document.queryForm.query.value;
